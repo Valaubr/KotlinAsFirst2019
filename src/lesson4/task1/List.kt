ce4c8c740410b7c33742f9976cf8799d1607d93b
Valaubr <stalker039906@gmail.com>
2019-09-15 18:37:40 +0300

    Полностью выполнен 4-й урок.

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -3,7 +3,12 @@
 package lesson4.task1
 
 import lesson1.task1.discriminant
+import lesson1.task1.sqr
+import org.junit.experimental.theories.internal.ParameterizedAssertionError.join
+import java.lang.Math.*
+import java.util.Collections.nCopies
 import kotlin.math.sqrt
+import kotlin.collections.arrayListOf as arrayListOf1
 
 /**
  * Пример
@@ -115,14 +120,33 @@ fun buildSumExample(list: List<Int>) = list.joinToString(separator = " + ", post
  * по формуле abs = sqrt(a1^2 + a2^2 + ... + aN^2).
  * Модуль пустого вектора считать равным 0.0.
  */
-fun abs(v: List<Double>): Double = TODO()
+fun abs(v: List<Double>): Double {
+    var x = 0.0;
+    return if (v.isEmpty()) {
+        0.0;
+    } else {
+        for (element in v) {
+            x += sqr(element);
+        }
+        sqrt(x);
+    }
+}
 
 /**
  * Простая
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>): Double {
+    var x = 0.0
+    if (list.isEmpty()) {
+        return 0.0;
+    }
+    for (element in list) {
+        x += element
+    }
+    return x / list.size;
+}
 
 /**
  * Средняя
@@ -132,7 +156,19 @@ fun mean(list: List<Double>): Double = TODO()
  *
  * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
  */
-fun center(list: MutableList<Double>): MutableList<Double> = TODO()
+fun center(list: MutableList<Double>): MutableList<Double> {
+    var i = 0;
+    var mean = mean(list)
+    if (list.isEmpty()) {
+        return list;
+    } else {
+        while (i < list.size) {
+            list[i] -= mean
+            i++;
+        }
+        return list;
+    }
+}
 
 /**
  * Средняя
@@ -141,7 +177,19 @@ fun center(list: MutableList<Double>): MutableList<Double> = TODO()
  * представленные в виде списков a и b. Скалярное произведение считать по формуле:
  * C = a1b1 + a2b2 + ... + aNbN. Произведение пустых векторов считать равным 0.
  */
-fun times(a: List<Int>, b: List<Int>): Int = TODO()
+fun times(a: List<Int>, b: List<Int>): Int {
+    var i = 0;
+    var c = 0;
+    return if (a.isEmpty() || b.isEmpty()) {
+        c;
+    } else {
+        while (i < a.size) {
+            c += a[i] * b[i];
+            i++;
+        }
+        c
+    }
+}
 
 /**
  * Средняя
@@ -151,7 +199,24 @@ fun times(a: List<Int>, b: List<Int>): Int = TODO()
  * Коэффициенты многочлена заданы списком p: (p0, p1, p2, p3, ..., pN).
  * Значение пустого многочлена равно 0 при любом x.
  */
-fun polynom(p: List<Int>, x: Int): Int = TODO()
+fun polynom(p: List<Int>, x: Int): Int {
+    var i = 0;
+    var z = 0.0;
+    return if (p.isEmpty()) {
+        0;
+    } else {
+        while (i < p.size) {
+            if (i == 0) {
+                z += p[i];
+                i++;
+            } else {
+                z += p[i] * pow(x.toDouble(), i.toDouble());
+                i++;
+            }
+        }
+        z.toInt()
+    }
+}
 
 /**
  * Средняя
@@ -163,7 +228,22 @@ fun polynom(p: List<Int>, x: Int): Int = TODO()
  *
  * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
  */
-fun accumulate(list: MutableList<Int>): MutableList<Int> = TODO()
+fun accumulate(list: MutableList<Int>): MutableList<Int> {
+    // да ну в кои то веки понял как forEach работает, странно правда что я не могу указать it += before,
+    // но раз можно сделать функцией, буду делать функцией...
+    var before = 0;
+    var i = 0;
+    if (list.isEmpty()) {
+        return list;
+    } else {
+        while (i < list.size) {
+            list[i] += before;
+            before = list[i];
+            i++;
+        }
+        return list;
+    }
+}
 
 /**
  * Средняя
@@ -172,7 +252,26 @@ fun accumulate(list: MutableList<Int>): MutableList<Int> = TODO()
  * Результат разложения вернуть в виде списка множителей, например 75 -> (3, 5, 5).
  * Множители в списке должны располагаться по возрастанию.
  */
-fun factorize(n: Int): List<Int> = TODO()
+fun factorize(n: Int): List<Int> {
+    var a = n;
+    var list = arrayListOf1<Int>()
+    var x = 2;
+    while (a > 1) {
+        while (a % x == 0) {
+            if (a != 2) {
+                a /= x
+                list.add(x);
+            } else {
+                list.add(x);
+                a /= x
+                break
+            }
+        }
+        if (x == 2) x++;
+        else x += 2;
+    }
+    return list.toList();
+}
 
 /**
  * Сложная
@@ -181,7 +280,16 @@ fun factorize(n: Int): List<Int> = TODO()
  * Результат разложения вернуть в виде строки, например 75 -> 3*5*5
  * Множители в результирующей строке должны располагаться по возрастанию.
  */
-fun factorizeToString(n: Int): String = TODO()
+fun factorizeToString(n: Int): String {
+    var x = factorize(n);
+    var i = 0;
+    var returnedStr = "";
+    while (i < x.size) {
+        returnedStr += x[i].toString() + "*"
+        i++;
+    }
+    return trim(returnedStr);
+}
 
 /**
  * Средняя
@@ -190,7 +298,36 @@ fun factorizeToString(n: Int): String = TODO()
  * Результат перевода вернуть в виде списка цифр в base-ичной системе от старшей к младшей,
  * например: n = 100, base = 4 -> (1, 2, 1, 0) или n = 250, base = 14 -> (1, 3, 12)
  */
-fun convert(n: Int, base: Int): List<Int> = TODO()
+fun convert(n: Int, base: Int): List<Int> {
+    var list = arrayListOf1<Int>();
+    var number = n;
+    var c = 0;
+    var d = 0;
+    if (number > 9) {
+        while (true) {
+            d = number % base
+            list.add(d)
+            c = number / base
+            number /= base
+            if ((number / base) == 0) {
+                list.add(number % base)
+                break
+            }
+        }
+    } else {
+        while (true) {
+
+            d = number % base
+            list.add(d)
+            c = number / base
+            number /= base
+            if ((number / base) == 0) {
+                break
+            }
+        }
+    }
+    return list.reversed().toList();
+}
 
 /**
  * Сложная
@@ -203,7 +340,22 @@ fun convert(n: Int, base: Int): List<Int> = TODO()
  * Использовать функции стандартной библиотеки, напрямую и полностью решающие данную задачу
  * (например, n.toString(base) и подобные), запрещается.
  */
-fun convertToString(n: Int, base: Int): String = TODO()
+fun convertToString(n: Int, base: Int): String {
+    // я чет подумал, зачем 2 раза писать одно и то же если можно воспользоваться написанной самим собой реализацией
+    var x = convert(n, base);
+    var notStandartNum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    var i = 0;
+    var finalString = "";
+    while (i < x.size) {
+        finalString += notStandartNum[x[i]].toString();
+
+        if (i == 0 && finalString == "0") {
+            finalString = "";
+        }
+        i++;
+    }
+    return finalString.toLowerCase();
+}
 
 /**
  * Средняя
@@ -212,7 +364,16 @@ fun convertToString(n: Int, base: Int): String = TODO()
  * из системы счисления с основанием base в десятичную.
  * Например: digits = (1, 3, 12), base = 14 -> 250
  */
-fun decimal(digits: List<Int>, base: Int): Int = TODO()
+fun decimal(digits: List<Int>, base: Int): Int {
+    var i = 0;
+    var finalNum = 0.0;
+    var digitsRevers = digits.reversed();
+    while (i < digits.size) {
+        finalNum += digitsRevers[i] * pow(base.toDouble(), i.toDouble())
+        i++;
+    }
+    return finalNum.toInt()
+}
 
 /**
  * Сложная
@@ -226,7 +387,56 @@ fun decimal(digits: List<Int>, base: Int): Int = TODO()
  * Использовать функции стандартной библиотеки, напрямую и полностью решающие данную задачу
  * (например, str.toInt(base)), запрещается.
  */
-fun decimalFromString(str: String, base: Int): Int = TODO()
+fun decimalFromString(str: String, base: Int): Int {
+    var x = "";
+    var num = arrayListOf1<Int>()
+    var i = 0;
+
+    while (i < str.length) {
+        num.add(
+            when (str[i].toString()) {
+                "1" -> 1;
+                "2" -> 2;
+                "3" -> 3;
+                "4" -> 4;
+                "5" -> 5;
+                "6" -> 6;
+                "7" -> 7;
+                "8" -> 8;
+                "9" -> 9;
+                "a" -> 10;
+                "b" -> 11;
+                "c" -> 12;
+                "d" -> 13;
+                "e" -> 14;
+                "f" -> 15;
+                "g" -> 16;
+                "h" -> 17;
+                "i" -> 18;
+                "j" -> 19;
+                "k" -> 20;
+                "l" -> 21;
+                "m" -> 22;
+                "n" -> 23;
+                "o" -> 24;
+                "p" -> 25;
+                "q" -> 26;
+                "r" -> 27;
+                "s" -> 28;
+                "t" -> 29;
+                "u" -> 30;
+                "v" -> 31;
+                "w" -> 32;
+                "x" -> 33;
+                "y" -> 34;
+                "z" -> 35;
+                else -> 0;
+            }
+        )
+        i++;
+    }
+    return decimal(num, base);
+}
 
 /**
  * Сложная
@@ -236,13 +446,167 @@ fun decimalFromString(str: String, base: Int): Int = TODO()
  * 90 = XC, 100 = C, 400 = CD, 500 = D, 900 = CM, 1000 = M.
  * Например: 23 = XXIII, 44 = XLIV, 100 = C
  */
-fun roman(n: Int): String = TODO()
+fun roman(n: Int): String {
+    //не бейте палками, я ещё в колледже решал такую задачку на Java так что реализация с Java библиотеками :)
+    //Принцип решения прост, каждую единицу мы представляем в виде "I" и заменяем "IIII" на "IV" и ТД по списку :)
+    return join("", nCopies(n, "I") as Collection<Any>?)
+        .replace("IIIII", "V")
+        .replace("IIII", "IV")
+        .replace("VV", "X")
+        .replace("VIV", "IX")
+        .replace("XXXXX", "L")
+        .replace("XXXX", "XL")
+        .replace("LL", "C")
+        .replace("LXL", "XC")
+        .replace("CCCCC", "D")
+        .replace("CCCC", "CD")
+        .replace("DD", "M")
+        .replace("DCD", "CM");
+}
 
 /**
  * Очень сложная
  *
- * Записать заданное натуральное число 1..999999 прописью по-русски.
+ * Записать заданное натуральное число 1..999 999 прописью по-русски.
  * Например, 375 = "триста семьдесят пять",
  * 23964 = "двадцать три тысячи девятьсот шестьдесят четыре"
  */
-fun russian(n: Int): String = TODO()
+fun russian(n: Int): String {
+
+    //Я приношу свои глубочайшие извинения, но читать реализацию через простые List`ы крайне не удобно,
+    //я просто оставлю реализацию через map
+
+    var thousands: Map<Int, String> = linkedMapOf(
+        1 to "тысяча ", 2 to "тысячи ", 3 to "тысячи ", 4 to "тысячи ", 5 to "тысяч ",
+        6 to "тысяч ", 7 to "тысяч ", 8 to "тысяч ", 9 to "тысяч ", 0 to "тысяч "
+    )
+
+    var hundreeds: Map<Int, String> = linkedMapOf(
+        1 to "сто ", 2 to "двести ", 3 to "триста ", 4 to "четыресто ", 5 to "пятьсот ",
+        6 to "шестьсот ", 7 to "семьсот ", 8 to "восемьсот ", 9 to "девятьсот ", 0 to ""
+    )
+    var tens: Map<Int, String> = linkedMapOf(
+        1 to "десять ", 2 to "двадцать ", 3 to "тридцать ", 4 to "сорок ", 5 to "пятьдесят ",
+        6 to "шестьдесят ", 7 to "семьдесят ", 8 to "восемьдесят ", 9 to "девяносто ", 0 to ""
+    )
+    var justNum: Map<Int, String> = linkedMapOf(
+        1 to "одна ", 2 to "две ", 3 to "три ", 4 to "четыре ", 5 to "пять ",
+        6 to "шесть ", 7 to "семь ", 8 to "восемь ", 9 to "девять ", 0 to ""
+    )
+    var notStandartNum: Map<Int, String> = linkedMapOf(
+        1 to "одиннадцать ", 2 to "двенадцать ", 3 to "тринадцать ", 4 to "четырнадцать ", 5 to "пятнадцать ",
+        6 to "шестнадцать ", 7 to "семнадцать ", 8 to "восемнадцать ", 9 to "девятнадцать ", 0 to "десять "
+    )
+    var exeptionInJustNum: Map<Int, String> = linkedMapOf(
+        1 to "один ", 2 to "два ", 3 to "три ", 4 to "четыре ", 5 to "пять ",
+        6 to "шесть ", 7 to "семь ", 8 to "восемь ", 9 to "девять ", 0 to ""
+    )
+
+    var x = n.toString();
+
+    if (n.toString().length > 5) {
+        if (x[1].toString().toInt() == 1 && x[4].toString().toInt() == 1) {
+            return trim(
+                hundreeds[x[0].toString().toInt()] +
+                        notStandartNum[x[2].toString().toInt()] +
+                        "тысяч " +
+                        hundreeds[x[3].toString().toInt()] +
+                        notStandartNum[x[5].toString().toInt()]
+            )
+        } else if (x[1].toString().toInt() == 1) {
+            return trim(
+                hundreeds[x[0].toString().toInt()] +
+                        notStandartNum[x[2].toString().toInt()] +
+                        "тысяч " +
+                        hundreeds[x[3].toString().toInt()] +
+                        tens[x[4].toString().toInt()] +
+                        exeptionInJustNum[x[5].toString().toInt()]
+            )
+        } else if (x[4].toString().toInt() == 1) {
+            return trim(
+                hundreeds[x[0].toString().toInt()] +
+                        tens[x[1].toString().toInt()] +
+                        justNum[x[2].toString().toInt()] +
+                        thousands[x[2].toString().toInt()] +
+                        hundreeds[x[3].toString().toInt()] +
+                        notStandartNum[x[5].toString().toInt()]
+            )
+        } else {
+            return trim(
+                hundreeds[x[0].toString().toInt()] +
+                        tens[x[1].toString().toInt()] +
+                        justNum[x[2].toString().toInt()] +
+                        thousands[x[2].toString().toInt()] +
+                        hundreeds[x[3].toString().toInt()] +
+                        tens[x[4].toString().toInt()] +
+                        exeptionInJustNum[x[5].toString().toInt()]
+            )
+        }
+    } else if (n.toString().length > 4) {
+        if (x[0].toString().toInt() == 1 && x[3].toString().toInt() == 1) {
+            return trim(
+                notStandartNum[x[1].toString().toInt()] +
+                        "тысяч " +
+                        hundreeds[x[2].toString().toInt()] +
+                        notStandartNum[x[3].toString().toInt()]
+            )
+        } else if (x[0].toString().toInt() == 1) {
+            return trim(
+                notStandartNum[x[1].toString().toInt()] +
+                        "тысяч " +
+                        hundreeds[x[2].toString().toInt()] +
+                        tens[x[3].toString().toInt()] +
+                        exeptionInJustNum[x[4].toString().toInt()]
+            )
+        } else if (x[3].toString().toInt() == 1) {
+            return trim(
+                tens[x[0].toString().toInt()] +
+                        justNum[x[1].toString().toInt()] +
+                        thousands[x[1].toString().toInt()] +
+                        hundreeds[x[2].toString().toInt()] +
+                        notStandartNum[x[3].toString().toInt()]
+            )
+        } else {
+            return trim(
+                tens[x[0].toString().toInt()] +
+                        justNum[x[1].toString().toInt()] +
+                        thousands[x[1].toString().toInt()] +
+                        hundreeds[x[2].toString().toInt()] +
+                        tens[x[3].toString().toInt()] +
+                        exeptionInJustNum[x[4].toString().toInt()]
+            )
+        }
+
+    } else if (n.toString().length > 3) {
+        if (x[3].toString().toInt() == 1) {
+            return trim(
+                justNum[x[0].toString().toInt()] + thousands[x[0].toString().toInt()] +
+                        hundreeds[x[1].toString().toInt()] + notStandartNum[x[3].toString().toInt()]
+            )
+        } else {
+            return trim(
+                justNum[x[0].toString().toInt()] + thousands[x[0].toString().toInt()] +
+                        hundreeds[x[1].toString().toInt()] + tens[x[2].toString().toInt()] +
+                        justNum[x[3].toString().toInt()]
+            )
+        }
+    } else if (n.toString().length > 2) {
+        return if (x[1].toString().toInt() == 1) {
+            trim(hundreeds[x[0].toString().toInt()] + notStandartNum[x[2].toString().toInt()])
+        } else {
+            trim(hundreeds[x[0].toString().toInt()] + tens[x[1].toString().toInt()] + exeptionInJustNum[x[2].toString().toInt()])
+        }
+
+    } else if (n.toString().length > 1) {
+        return if (x[0].toString().toInt() == 1) {
+            trim(notStandartNum[x[1].toString().toInt()].toString())
+        } else {
+            trim(tens[x[0].toString().toInt()] + exeptionInJustNum[x[1].toString().toInt()])
+        }
+    } else {
+        trim(exeptionInJustNum[x.toInt()].toString())
+    }
+    return ""
+}
+
+fun trim(n: String): String = n.substring(0, n.length - 1)
